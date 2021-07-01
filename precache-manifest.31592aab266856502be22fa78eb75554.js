cbf6019fc45dcddd2107da864ccbd84aaf755cb6
chaos430 <1192318530@qq.com>
2021-07-01 15:20:35 +0800

    deploy

---
+++ precache-manifest.31592aab266856502be22fa78eb75554.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dc3897a9968a40a4f3",
+    "url": "/lemoney-website-2/css/app.f8b5baf7.css"
+  },
+  {
+    "revision": "a2c148a44a64d9cc8158",
+    "url": "/lemoney-website-2/css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "6a7235e1641fb8f3da6be1d410d97567",
+    "url": "/lemoney-website-2/index.html"
+  },
+  {
+    "revision": "d5dc3897a9968a40a4f3",
+    "url": "/lemoney-website-2/js/app.c71bbb49.js"
+  },
+  {
+    "revision": "a2c148a44a64d9cc8158",
+    "url": "/lemoney-website-2/js/chunk-vendors.47d0aca5.js"
+  },
+  {
+    "revision": "1c63b790d8f61dada5263cf9ec3111d9",
+    "url": "/lemoney-website-2/manifest.json"
+  },
+  {
+    "revision": "e17abca263aa625c5e3fad4e32b81867",
+    "url": "/lemoney-website-2/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lemoney-website-2/robots.txt"
+  }
+]);
